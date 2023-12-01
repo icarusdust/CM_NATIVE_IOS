7c0ba1d20d696588a505ef31ef220a271d6e812c
Nihal <nihalagazade@gmail.com>
2023-12-01 17:44:41 +0200

    Initial Commit

---
+++ CM_NATIVE_IOS/CM_NATIVE_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CM_NATIVE_IOSApp.swift
+//  CM_NATIVE_IOS
+//
+//  Created by nihalagazade on 01.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct CM_NATIVE_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CM_NATIVE_IOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CM_NATIVE_IOS
+//
+//  Created by nihalagazade on 01.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
